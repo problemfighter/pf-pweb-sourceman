e9d4f6df2435f1a5f876b5c160a4483a4694d2bb
hmtmcse <hmtm.cse@gmail.com>
2022-03-16 09:44:14 +0600

    updated version

--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ def get_dependencies():
     if env and env == "dev":
         return dependency
 
-    return dependency + ['PF-PY-YMLEnv']
+    return dependency + ['PF-PY-YMLEnv', 'PF-PY-File']
 
 
 setup(
     name='PF-PWeb-SourceMan',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/problemfighter/pf-pweb-sourceman',
     license='Apache 2.0',
     author='Problem Fighter',
