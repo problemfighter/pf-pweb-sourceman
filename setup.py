4f31b11f1fa09f4c6826d41d3cb1f06aed534009
hmtmcse <hmtm.cse@gmail.com>
2022-03-16 10:11:34 +0600

    updated version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_dependencies():
 
 setup(
     name='PF-PWeb-SourceMan',
-    version='1.0.1',
+    version='1.0.3',
     url='https://github.com/problemfighter/pf-pweb-sourceman',
     license='Apache 2.0',
     author='Problem Fighter',
