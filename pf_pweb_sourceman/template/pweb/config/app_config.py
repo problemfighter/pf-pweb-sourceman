fd2a2f1cad3b9694646ae5e0db2e9e5ccca8bf38
hmtmcse <hmtm.cse@gmail.com>
2022-06-04 21:50:51 +0600

    initiate project

--- pf_pweb_sourceman/template/pweb/config/app_config.py
+++ pf_pweb_sourceman/template/pweb/config/app_config.py
@@ -1,3 +1,23 @@
 class Config:
     APP_NAME = "__APP_NAME__"
     PORT: int = __APP_PORT__
+
+    ENABLE_AUTH_SYSTEM: bool = True
+
+    # Swagger Configuration
+    ENABLE_SWAGGER_VIEW_PAGE: bool = True
+    ENABLE_SWAGGER_PAGE_AUTH: bool = False
+    SWAGGER_ENABLE: bool = True
+    SWAGGER_TITLE: str = "__APP_NAME__"
+
+    # API
+    ENABLE_API_AUTH: bool = True
+    ENABLE_API_END_POINTS: bool = True
+    SWAGGER_ENABLE_JWT_AUTH_GLOBAL: bool = True
+
+    # JWT Token validity
+    JWT_REFRESH_TOKEN_VALIDITY_MIN: int = (60 * 24)
+    JWT_ACCESS_TOKEN_VALIDITY_MIN: int = (60 * 24 * 2)
+
+    # Custom Operator System
+    ENABLE_DEFAULT_AUTH_MODEL: bool = False
